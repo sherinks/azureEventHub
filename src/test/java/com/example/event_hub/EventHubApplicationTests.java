79f8e9bd0d9c58d13d733993ee112dc4783ad9fa
SSyriac <ssyriac@archinsurance.com>
2025-03-03 14:05:03 -0500

    Initial setup

---
+++ src/test/java/com/example/event_hub/EventHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.event_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
